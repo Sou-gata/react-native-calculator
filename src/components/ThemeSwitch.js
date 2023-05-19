dfb7cc6abd374e601bba650e5e5b548510cbfcfa
Sougata Talukdar <sougatatalukdar77@gmail.com>
2023-05-19 14:34:09 +0530

    animate theme change icon

--- src/components/ThemeSwitch.js
+++ src/components/ThemeSwitch.js
@@ -1,44 +1,108 @@
-import { View, Pressable } from "react-native";
-import React from "react";
+import { View, Pressable, Animated, StyleSheet } from "react-native";
+import { useRef } from "react";
 import { MaterialIcons, Ionicons } from "@expo/vector-icons";
 import { useSelector, useDispatch } from "react-redux";
 import { switchMode } from "../../redux-store/actions";
 
 const ThemeSwitch = ({ colors }) => {
-    const themeMode = useSelector((state) => state.theme);
+    const animOne = useRef(new Animated.Value(0)).current;
+    const animTwo = useRef(new Animated.Value(1)).current;
+    const mode = useSelector((state) => state.theme.mode);
     const dispatch = useDispatch();
+    const lightIn = () => {
+        Animated.timing(animOne, {
+            toValue: 1,
+            duration: 500,
+            useNativeDriver: true,
+        }).start();
+        Animated.timing(animTwo, {
+            toValue: 0,
+            duration: 500,
+            useNativeDriver: true,
+        }).start();
+    };
+    const rotateOne = animOne.interpolate({
+        inputRange: [0, 1],
+        outputRange: ["0deg", "360deg"],
+    });
+
+    const darkIn = () => {
+        Animated.timing(animOne, {
+            toValue: 0,
+            duration: 500,
+            useNativeDriver: true,
+        }).start();
+        Animated.timing(animTwo, {
+            toValue: 1,
+            duration: 500,
+            useNativeDriver: true,
+        }).start();
+    };
+
+    const rotateTwo = animTwo.interpolate({
+        inputRange: [0, 1],
+        outputRange: ["0deg", "360deg"],
+    });
     return (
         <View
             style={{
                 position: "absolute",
                 zIndex: 99,
-                right: 0,
+                right: 16,
                 top: 10,
-                width: 40,
-                height: 40,
+                width: 24,
+                height: 24,
             }}
         >
             <Pressable
                 onPress={() => {
-                    dispatch(
-                        switchMode(
-                            themeMode.mode === "light" ? "dark" : "light"
-                        )
-                    );
+                    if (mode == "dark") lightIn();
+                    else darkIn();
+                    dispatch(switchMode(mode === "light" ? "dark" : "light"));
                 }}
             >
-                {themeMode.mode === "light" ? (
+                <Animated.View
+                    style={[
+                        styles.iconContainer,
+                        {
+                            opacity: animOne,
+                            transform: [
+                                { scale: animOne },
+                                { rotate: rotateOne },
+                            ],
+                        },
+                    ]}
+                >
                     <MaterialIcons
                         name="brightness-7"
                         size={24}
                         color={colors.secondary}
                     />
-                ) : (
+                </Animated.View>
+                <Animated.View
+                    style={[
+                        styles.iconContainer,
+                        {
+                            opacity: animTwo,
+                            transform: [
+                                { scale: animTwo },
+                                { rotate: rotateTwo },
+                            ],
+                        },
+                    ]}
+                >
                     <Ionicons name="moon" size={24} color={colors.secondary} />
-                )}
+                </Animated.View>
             </Pressable>
         </View>
     );
 };
+const styles = StyleSheet.create({
+    iconContainer: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+    },
+});
 
 export default ThemeSwitch;
